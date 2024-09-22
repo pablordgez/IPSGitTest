f1195682d1e0d025dc953ca4906c421706c758d4
Pablo Rodríguez <85810125+pablordgez@users.noreply.github.com>
2024-09-22 21:08:35 +0200

    Added changes to be rebased (from branch2)

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -10,5 +10,6 @@ public class Demo {
     System.out.println("hello from branch2");
     System.out.println("hello from master");
     System.out.println("this will be rebased (from master");
+    System.out.println("this will be rebased! (from branch2)");
   }
 }
